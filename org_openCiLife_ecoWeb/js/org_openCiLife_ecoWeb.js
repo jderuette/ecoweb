97b7c6a0076de64f56e127ed2763225338fd0b58
jeremie.deruette <djer13@gmail.com>
2025-08-23 16:17:59 +0200

    Site checkin for project SiteBuilder

--- org_openCiLife_ecoWeb/js/org_openCiLife_ecoWeb.js
+++ org_openCiLife_ecoWeb/js/org_openCiLife_ecoWeb.js
@@ -37,3 +37,54 @@ $('#basicModal').on('show.bs.modal', function (event) {
 $('*[data-href]').on('click', function() {
     window.location = $(this).data("href");
 });
+
+function changeValue(targetId, newValue) {
+  var input1 = document.getElementById(targetId);
+  input1.value = newValue;
+}
+
+$('*[data-obfuscatedMailTo]').on('submit', function() {
+	console.log ("data-obfuscatedMailTo : Handling obfuscated action for form : " + $(this).attr('id'));
+	const obfuscatedEmail = $(this).data("obfuscatedmailto");
+	const obfuscatedEmailKey = $(this).data("obfuscatedmailtokey");
+	console.log ("data-obfuscatedMailTo : prepare to unobfuscate data : " + obfuscatedEmail + " using key : " + obfuscatedEmailKey);
+	
+	const humanReadeableEmail = unObfuscText(obfuscatedEmail, obfuscatedEmailKey);
+	const newAction = "mailto:"+humanReadeableEmail
+	$(this).attr('action', newAction);
+    return true;
+});
+
+
+$('*[data-obfuscatedValue]').on('click', function() {
+	const obfuscatedValue = $(this).data("obfuscatedvalue");
+	const obfuscatedKey = $(this).data("obfuscatedkey");
+	const obfuscatedTarget = $(this).data("obfuscatedtarget");
+	console.log ("data-obfuscatedValue : Handling obfuscated action for : " + obfuscatedTarget  + " ("+obfuscatedValue+" using key : "+obfuscatedKey+") cliked on : " + $(this).attr('id'));
+	const humanReadeableText = unObfuscText(obfuscatedValue, obfuscatedKey);
+	$(obfuscatedTarget).val(humanReadeableText);
+	$(obfuscatedTarget).attr("readonly", false);
+});
+
+$('span[data-obfuscatedkey]').on('click', function() {
+	const obfuscatedValue = $(this).text();
+	const obfuscatedKey = $(this).data("obfuscatedkey");
+	console.log ("data-obfuscatedText : Handling obfuscated ("+obfuscatedValue+" using key : "+obfuscatedKey+")");
+	const humanReadeableText = unObfuscText(obfuscatedValue, obfuscatedKey);
+	$(this).text(humanReadeableText);
+	$(this).off('click');
+	$(this).next('img.showHiddenDataAppendedButton').remove();
+});
+
+$('span[data-obfuscatedkey]').append('<img class="showHiddenDataAppendedButton" style="width: 24px;margin: 0 4px 0 4px;" src="file:///D:/git/CitoyenBlog/website/org_openCiLife_ecoWeb/images/eyes.svg" alt="clickez pour voir l\'e-mail en clair"></img>');
+
+function unObfuscText(source, key){
+	var unOfuscatedText = source;
+	const tokens = key.split(",");
+	for (const token of tokens){
+		const tokenDetails = token.split(":")
+		//console.log ("unObfuscText : Replacing : " + tokenDetails[1] + " by : " + tokenDetails[0]);
+		unOfuscatedText = unOfuscatedText.replace(tokenDetails[1], tokenDetails[0]);
+	}
+	return unOfuscatedText;
+}
