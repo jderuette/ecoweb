920cf6815fa22c79ee745c75cc6a7565125fc163
jeremie.deruette <djer13@gmail.com>
2025-08-23 16:17:27 +0200

    Better placement of image for hide span

--- jBakeSite/assets/org_openCiLife_ecoWeb/js/org_openCiLife_ecoWeb.js
+++ jBakeSite/assets/org_openCiLife_ecoWeb/js/org_openCiLife_ecoWeb.js
@@ -76,7 +76,7 @@ $('span[data-obfuscatedkey]').on('click', function() {
 	$(this).next('img.showHiddenDataAppendedButton').remove();
 });
 
-$('span[data-obfuscatedkey]').after('<img class="showHiddenDataAppendedButton" style="width: 24px;margin: 0 4px 0 4px;" src="${webleger.build.host}/${webleger.site.template}/images/eyes.svg" alt="clickez pour voir l\'e-mail en clair"></img>');
+$('span[data-obfuscatedkey]').append('<img class="showHiddenDataAppendedButton" style="width: 24px;margin: 0 4px 0 4px;" src="${webleger.build.host}/${webleger.site.template}/images/eyes.svg" alt="clickez pour voir l\'e-mail en clair"></img>');
 
 function unObfuscText(source, key){
 	var unOfuscatedText = source;
